b2dcfd61a7b7ba135598a3886c49ff2762b74f2d
Mustafa Günalp <mustafa.gunalp@hisarschool.k12.tr>
2022-12-16 19:23:08 +0300

    plot wave file function

--- src/util.jl
+++ src/util.jl
@@ -8,7 +8,7 @@ function freq_to_midi(freq::Float64)
     return round(Int, midi_number)
 end
 
-function output_periodogram(filepath::String)
+function plot_periodogram(filepath::String)
     signal, fs = wavread(filepath)
 
     signal_array = signal[1:end]
@@ -17,10 +17,39 @@ function output_periodogram(filepath::String)
     power = a.power
     freq = a.freq
     plot(freq, power)
-
-
 end
 
-#output_periodogram("src/PinkPanther30.wav")
+div_array(arr, n) = [arr[i:min(i + n - 1, end)] for i in 1:n:length(arr)]
 
-println(freq_to_midi(131.00))
+function plot_waveform(filepath::String)
+    signal, fs = wavread(filepath)
+    signal_array = signal[1:end]
+
+    """n_samples = length(signal[1:end])
+    duration_floored = trunc(Int64, (n_samples/fs))
+    seconds = Array{Int64}(undef, duration_floored)
+
+    for i in 1:duration_floored
+        seconds[i] = fs*i
+    end"""
+
+    seconds = range(1, step = fs, length = length(signal_array))
+
+    plot(seconds, signal_array)
+end
+
+
+"""signal, fs = wavread("src/PinkPanther30.wav")
+signal_array = signal[1:end]
+a = div_array(signal_array, 512)
+
+println((a[1292]))"""
+
+plot_waveform("src/PinkPanther30.wav")
+
+
+
+
+#plot_periodogram("src/PinkPanther30.wav")
+
+
