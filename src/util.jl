b92b7b23fe3a5197334c7244082aede36b170f9a
gunalpms <80674364+gunalpms@users.noreply.github.com>
2023-01-16 14:18:36 +0300

    functions fixed

--- src/util.jl
+++ src/util.jl
@@ -2,12 +2,14 @@ using DSP
 using WAV
 using Plots
 
+#convert given frequency to its midi note
 function freq_to_midi(freq::Float64)
     midi_number = 12 * log2(freq / 440) + 69
 
     return round(Int, midi_number)
 end
 
+#plot the entire periodogram of a given wav file (filepath)
 function plot_periodogram(filepath::String)
     signal, fs = wavread(filepath)
 
@@ -17,39 +19,34 @@ function plot_periodogram(filepath::String)
     power = a.power
     freq = a.freq
     plot(freq, power)
+    savefig("periodogram.png")
 end
 
-div_array(arr, n) = [arr[i:min(i + n - 1, end)] for i in 1:n:length(arr)]
+# return arr divided into parts with n elements, rounds the last list down 
+# for example, the last element of a 9 length arr will be of length 2 if it is wanted to be split into
+# elements of length 3
+function div_array(arr, n) 
+    return [arr[i:min(i + n - 1, end)] for i in 1:n:length(arr)]
+end
 
+
+# plot waveform of a given wav file in its filepath
 function plot_waveform(filepath::String)
     signal, fs = wavread(filepath)
     signal_array = signal[1:end]
 
-    """n_samples = length(signal[1:end])
-    duration_floored = trunc(Int64, (n_samples/fs))
-    seconds = Array{Int64}(undef, duration_floored)
+    
+    len_seconds = Int(div(length(signal_array), fs))
+    seconds_array = collect(range(0, step = len_seconds/length(signal_array), length = length(signal_array)))
+    println(seconds_array[end-5:end])
+    println(length(signal_array))
 
-    for i in 1:duration_floored
-        seconds[i] = fs*i
-    end"""
-
-    seconds = range(1, step = fs, length = length(signal_array))
-
-    plot(seconds, signal_array)
+    plot(seconds_array, signal_array)
+    savefig("waveform.png")
 end
 
 
-"""signal, fs = wavread("src/PinkPanther30.wav")
-signal_array = signal[1:end]
-a = div_array(signal_array, 512)
-
-println((a[1292]))"""
-
-plot_waveform("src/PinkPanther30.wav")
 
 
-
-
-#plot_periodogram("src/PinkPanther30.wav")
 
 
